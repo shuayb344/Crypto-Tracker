5f3cd78da16fe5050cee2057261108124cfa4c10
Shuayb Mohammed <shuayb.mohammed@astu.edu.et>
2026-02-28 14:25:04 +0300

    styling homePage cards

--- cryptoPriceTracker/src/components/HomePageCards.jsx
+++ cryptoPriceTracker/src/components/HomePageCards.jsx
@@ -29,21 +29,24 @@ export function HomePageCards() {
   return (
     <div className="flex flex-col sm:flex-row gap-3">
       {data.slice(0, 4).map((crypto) => (
-        <div className="bg-neutral-950 mx-auto p-4 hover:bg-neutral-900 rounded-lg shadow-md flex flex-row sm:flex-col items-center  gap-4 mb-3 ">
+        <div className="bg-neutral-950 mx-auto p-6 hover:bg-neutral-900 rounded-lg shadow-md flex flex-row sm:flex-col justify-between items-center w-full gap-4 mb-3 ">
+          <div className="flex sm:flex-col flex-row items-center gap-2">
           <span className="">#{crypto.market_cap_rank}</span>
           <img className="w-12  h-12 sm:w-24 sm:h-24" src={crypto.image} alt={crypto.name} />
-          <div>
-            <h2 className="text-lg font-bold">{crypto.name}</h2>
-            <p className="text-sm text-gray-400">
+          <div className="flex flex-col items-start sm:items-center gap-1">
+            <h2 className="text-lg text-center font-bold">{crypto.name}</h2>
+            <p className="text-sm text-center text-gray-400">
               {crypto.symbol.toUpperCase()}
             </p>
           </div>
-          <div className="ml-auto flex flex-col items-end">
-            <p className="text-lg font-semibold">
+            
+          </div>
+          <div className=" flex flex-col items-end sm:items-center gap-1">
+            <p className="text-lg  font-semibold">
               {formatMoney(crypto.current_price)}
             </p>
             <p
-              className={`text-sm ${
+              className={`text-sm text-center ${
                 crypto.price_change_percentage_24h >= 0
                   ? "text-green-500"
                   : "text-red-500"
