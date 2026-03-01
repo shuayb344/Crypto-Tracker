14e944b614c32fc67d6493f28b8feacc5a384603
Shuayb Mohammed <shuayb.mohammed@astu.edu.et>
2026-03-01 08:56:34 +0300

    installing recharts

--- cryptoPriceTracker/src/pages/CryptoDetailsPage.jsx
+++ cryptoPriceTracker/src/pages/CryptoDetailsPage.jsx
@@ -35,13 +35,13 @@ export function CryptoDetailsPage() {
             <div className="w-12 h-12  border-4 border-gray-500 border-t-transparent rounded-full animate-spin "></div>
             <p>Loading...</p>
           </div>
-        ) : data && data.id ? (
+        ) : data && data.id ? (<>
           <div className="p-6 bg-neutral-950 rounded-lg mt-4">
-            <div className="flex flex-col sm:flex-row gap-6 items-center">
+            <div className="flex flex-col sm:flex-row gap-6 flex-wrap justify-between items-center">
                 <img src={data.image?.large } alt={data.name} />
                 <div>
                   <p className="text-lg mb-2">Rank: #{data.market_cap_rank}</p>
-                  <h2 className="text-3xl font-bold mb-2">{data.name} ({data.symbol?.toUpperCase()})</h2>
+                  <h2 className="sm:text-3xl text-2xl font-bold mb-2">{data.name} ({data.symbol?.toUpperCase()})</h2>
                   <p className="text-3xl mt-3"> {formatMoney(data.market_data?.current_price?.usd)}</p>
                    <p
               className={`text-lg p-2 ${
@@ -53,19 +53,26 @@ export function CryptoDetailsPage() {
               {data.market_data?.price_change_percentage_24h?.toFixed(2)}%
             </p>
                 </div>
-                <div>
-                  <h3 className="text-3xl font-bold mb-2">Market Cap </h3>
-                  <p>{formatMarketCap(data.market_data?.market_cap?.usd)}</p>
+                <div className="flex sm:flex-col flex-row gap-4 mt-6 sm:mt-0">
+                  <div>
+                  <h3 className="sm:text-3xl text-2xl  font-bold mb-2">Market Cap </h3>
+                  <p className="text-center sm:text-xl" >{formatMarketCap(data.market_data?.market_cap?.usd)}</p>
                 </div>
                 <div>
-                  <h3 className="text-3xl font-bold mb-2">24h Volume </h3>
-                  <p>{formatMarketCap(data.market_data?.total_volume?.usd)}</p>
+                  <h3 className="sm:text-3xl text-2xl font-bold mb-2">24h Volume </h3>
+                  <p className="text-center sm:text-xl">{formatMarketCap(data.market_data?.total_volume?.usd)}</p>
                 </div>
+                </div>
+                
                 
             </div>
             
             
           </div>
+          <div className="p-6 bg-neutral-950 rounded-lg mt-4">
+
+          </div>
+          </>
         ) : (<>
             <p className="text-center text-3xl ">Unable to load details for {id}</p>
             <button className="mt-4 mx-auto block bg-yellow-500 hover:bg-yellow-600 text-black font-bold py-2 px-4 rounded" onClick={() => navigate("/market")}>
