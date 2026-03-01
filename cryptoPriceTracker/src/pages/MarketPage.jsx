d540e31036b75491d25f66bdf9dc7bfc326abab3
Shuayb Mohammed <shuayb.mohammed@astu.edu.et>
2026-03-01 10:45:11 +0300

    moving search icon to public

--- cryptoPriceTracker/src/pages/MarketPage.jsx
+++ cryptoPriceTracker/src/pages/MarketPage.jsx
@@ -62,7 +62,7 @@ export function MarketPage() {
          <Link to="/"><span className='text-3xl font-bold text-yellow-500 font-["Dancing_Script"] '>CryptoTracker</span></Link>
         <div className="flex gap-2 items-center justify-center sm:w-1/2 border border-stone-700 bg-neutral-950 rounded-xl mt-3 align-middle">
           <input className="sm:p-3 p-2 rounded-xl   bg-neutral-950 w-[90%] focus:outline-0 text-white"  type="text" placeholder="Search Crypto..." value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}/>
-          <img src="./src/assets/search.png" alt="Search Icon" className="w-5 h-5 mr-2" />
+          <img src="./search.png" alt="Search Icon" className="w-5 h-5 mr-2" />
          
         </div>
          
