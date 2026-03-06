88bd6bb42a31cddd8ab2efffe253158cebae4a66
Shuayb Mohammed <shuayb.mohammed@astu.edu.et>
2026-03-06 17:07:24 +0300

    Integrate React Query into main application for improved data management

--- cryptoPriceTracker/src/main.jsx
+++ cryptoPriceTracker/src/main.jsx
@@ -1,14 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "./index.css";
+import App from "./App.jsx";
+const queryClient = new QueryClient();
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </QueryClientProvider>
   </BrowserRouter>,
-)
+);
